8822ff8b724bd9752aca1c339d3c10fbd9519fac
unknown <henrique_eduardo_souza@hotmail.com>
2023-02-03 19:56:27 -0300

    crypto fuctions

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,12 +1,15 @@
+var crypto = require('crypto');
+
 /**
  * adiciona o usuário na tabela UserCredentials.
  * @param {string} con conexão com o banco de dados sql.
  * @param {string} email email informado pelo usuário, que deve ser único.
  * @param {string} password senha informada pelo usuário.
  */
-export function add_user(con, email, password){
+function add_user_credentials(con, email, password){
     var sql_query = "INSERT INTO UserCredentials(email, password, salt) VALUES ?";
-    var values = [[email, password, "fdboinjg894"]];
+    var salt = new_salt(64);
+    var values = [[sha256(email), sha256(password + salt), salt]];
 
     con.query(sql_query, [values], (error, result, fields) => {
         if(error) return console.log(error);
@@ -20,11 +23,37 @@ export function add_user(con, email, password){
  * adiciona o usuário na tabela UserCredentials.
  * @param {string} con conexão com o banco de dados sql.
  * @param {string} email email informado pelo usuário.
+ * @returns {boolean} true significa que a autenticação foi bem sucedida; false, caso contrário.
+ */
+function get_salt(con, email){
+    var search_condition = ["WHERE email = '", sha256(email), "'"].join("");
+    var sql_query = "SELECT salt FROM UserCredentials " + search_condition;
+    var salt = '';
+
+    console.log(sha256(email));
+
+    con.query(sql_query, function(error, result, fields) {
+        if(error) throw error;
+        console.log(result);
+        if(result.length == 0){
+            console.log("não encontrado");
+        } else {
+            return true;
+        }
+        con.end();
+    });
+    return salt;
+}
+
+/**
+ * adiciona o usuário na tabela UserCredentials.
+ * @param {string} con conexão com o banco de dados sql.
+ * @param {string} email email informado pelo usuário.
  * @param {string} password senha enviada pelo usuário.
  * @returns {boolean} true significa que a autenticação foi bem sucedida; false, caso contrário.
  */
-export function autheticate_user(con, email, password){
-    var search_condition = ["WHERE email = '", email].join("");
+function autheticate_credentials(con, email, password){
+    var search_condition = ["WHERE email = '", email, "'"].join("");
     var sql_query = "SELECT email, password, salt FROM UserCredentials " + search_condition;
 
     con.query(sql_query, function(error, result, fields) {
@@ -35,7 +64,6 @@ export function autheticate_user(con, email, password){
         } else {
             console.log("autenticação bem sucedida!");
             return true;
-            
         }
         con.end();
     });
@@ -43,4 +71,14 @@ export function autheticate_user(con, email, password){
     return false;
 }
 
-export {add_user, autheticate_user}
+function sha256(content){
+    return crypto.createHash('sha256').update(content).digest('hex')
+}
+
+function new_salt(length){
+    return crypto.randomBytes(Math.ceil(length/2))
+    .toString('hex')
+    .slice(0,length);
+};
+
+module.exports = {add_user_credentials, autheticate_credentials, get_salt};
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var mysql = require('mysql2');
 var app = express();
+var crypto = require('crypto');
+
+const UserControler = require("./Controllers/UserController");
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -13,5 +16,14 @@ con.connect(function(err) {
     if (err) throw err;
     console.log("Conexão com banco de dados SQL bem sucedida!");
 });
+
+var email = "henrique_eduardo_souza@hotmail.com";
+var password = "gatodebotas";
+
+// UserControler.add_user_credentials(con, email, password);
+
+// UserControler.autheticate_credentials(con, email, password);
+
+var salt = UserControler.get_salt(con, email);
 
 app.listen(3030);
